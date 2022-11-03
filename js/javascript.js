474226c339a2ba0afa6a43409bd038d97ee2b967
Sambo177 <samuel78kennedynz@gmail.com>
2022-11-03 18:22:08 +1300

    work

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
-//Sets the total to the starting value
-$('#total').val('85.00');
+//Sets the service fee at the start
+$('#serviceFee').val('85.00');
 
 //Warranty
 $('#warranty').change(function() {
@@ -101,18 +101,142 @@ $('#businessType').click(function() {
 	$('#bond').val(0)
 });
 
-
-//Prevent Future Date
-$(document).ready(function () {
-	var today = new Date();
-	var day=today.getDate()>9?today.getDate():"0"+today.getDate(); 
-	var month=(today.getMonth()+1)>9?(today.getMonth()+1):"0"+(today.getMonth()+1);
-	var year=today.getFullYear();
-
-	$("#purchaseDate").attr('max', year + "-" + month + "-" + day);
-	$("#repairDate").attr('max', year + "-" + month + "-" + day);
+//Currency set
+$("input[data-type='currency']").on({
+    keyup: function() {
+      formatCurrency($(this));
+    },
+    blur: function() { 
+      formatCurrency($(this), "blur");
+    }
 });
 
+function formatNumber(n) {
+  // format number 1000000 to 1,234,567
+  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+
+function formatCurrency(input, blur) {
+  // appends $ to value, validates decimal side
+  // and puts cursor back in right position.
+  
+  // get input value
+  var input_val = input.val();
+  
+  // don't validate empty input
+  if (input_val === "") { return; }
+  
+  // original length
+  var original_len = input_val.length;
+
+  // initial caret position 
+  var caret_pos = input.prop("selectionStart");
+    
+  // check for decimal
+  if (input_val.indexOf(".") >= 0) {
+
+    // get position of first decimal
+    // this prevents multiple decimals from
+    // being entered
+    var decimal_pos = input_val.indexOf(".");
+
+    // split number by decimal point
+    var left_side = input_val.substring(0, decimal_pos);
+    var right_side = input_val.substring(decimal_pos);
+
+    // add commas to left side of number
+    left_side = formatNumber(left_side);
+
+    // validate right side
+    right_side = formatNumber(right_side);
+    
+    // On blur make sure 2 numbers after decimal
+    if (blur === "blur") {
+      right_side += "00";
+    }
+    
+    // Limit decimal to only 2 digits
+    right_side = right_side.substring(0, 2);
+
+    // join number by .
+    input_val = "$" + left_side + "." + right_side;
+
+  } else {
+    // no decimal entered
+    // add commas to number
+    // remove all non-digits
+    input_val = formatNumber(input_val);
+    input_val = "$" + input_val;
+    
+    // final formatting
+    if (blur === "blur") {
+      input_val += ".00";
+    }
+  }
+  
+  // send updated string to input
+  input.val(input_val);
+
+  // put caret back in the right position
+  var updated_len = input_val.length;
+  caret_pos = updated_len - original_len + caret_pos;
+  input[0].setSelectionRange(caret_pos, caret_pos);
+}
+
+//Total + GST Calculation
+$('#addBtn, #removeBtn').click(function () {
+	var bondValue = $('#bond').val();
+	var serviceValue = $('#serviceFee').val();
+
+	var totalPrice = ((+bondValue) + (+serviceValue));
+	var fullGST = (totalPrice / 20) * 3;
+	var completePrice = ((+totalPrice) + (+fullGST));
+
+	$('#total').val(totalPrice);
+	$('#GST').val(fullGST);
+	$('#total_With_GST').val(completePrice);
+});
+
+$('#customerType, #businessType, #warranty').change(function() {
+	var bondValue = $('#bond').val();
+	var serviceValue = $('#serviceFee').val();
+
+	var totalPrice = ((+bondValue) + (+serviceValue));
+	var fullGST = (totalPrice / 20) * 3;
+	var completePrice = ((+totalPrice) + (+fullGST));
+
+	$('#total').val(totalPrice);
+	$('#GST').val(fullGST);
+	$('#total_With_GST').val(completePrice);
+});
+
+//Get the current date and set it as the maximum possible value for the purchase date input box.
+$('#submitBtn').click(function () {
+    var date = new Date();
+    var day = date.getDate()>9?date.getDate():"0"+date.getDate();
+    var month = (date.getMonth()+1)>9?(date.getMonth()+1):"0"+(date.getMonth()+1);
+    var year = date.getFullYear();
+
+    //Full date in one variable
+    var fullDate = (year + "-" + month + "-" + day)
+
+    $("#purchaseDate").attr('max', fullDate);
+
+    var end = $('#purchaseDate').val();
+    $("#repairDate").attr('min', end);
+    $("#repairDate").attr('max', fullDate);
+});
+
+//Updates when reset button is clicked
+$('#resetBtn').click(function () {
+	//Remove all added rows that have the classname = "newSelectedItem"
+	$('.newSelectedItem').remove();
+
+	//Update the appState
+	appState.courtesyPhone = {item: 'none', bond: 0};
+	appState.courtesyCharger = {item: 'none', bond: 0};
+});
 
 //JQUERY: FAQ SCRIPTS
 let proxy = 'https://cors-anywhere.herokuapp.com/';
